b803dc33846533789ac2e87cb1f39b8a617bc51b
wikig <carzygodwu@gmail.com>
2025-01-21 10:03:52 +0000

    update sdk

--- core/utills.ts
+++ core/utills.ts
@@ -1,7 +1,7 @@
 function formatTimeInterval(seconds:number) {
-  const days = Math.floor(seconds / (24 * 60 * 60));
-  const hours = Math.floor((seconds % (24 * 60 * 60)) / (60 * 60));
-  const minutes = Math.floor((seconds % (60 * 60)) / 60); 
+  const days = Math.floor(seconds /86400);
+  const hours = Math.floor((seconds % 86400) / (3600));
+  const minutes = Math.floor((seconds % (3600)) / 60); 
   const remainingSeconds = seconds % 60;
   return {
     days,
